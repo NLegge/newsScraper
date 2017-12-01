4468e4c815fa8d20b604bb1e4694796a362fe600
Your NameNLegge <little.legge@gmail.com>
2017-12-01 16:30:49 -0700

    Articles posting. Summary showing undefined.

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -7,15 +7,24 @@ $(document).on("click", "#scrape", function() {
 
     $.getJSON("/articles", function (data) {
       // For each one
-      for (let i = 0; i < data.length; i++) {
+      console.log(data);
+      for (let i = 0; i < 20; i++) {
         // Display the apropos information on the page
         $("#articles").append(
-          "<div class='list - group'></div><a href=" + data[i].URL + 
-          "class='list - group - item list - group - item - action flex - column align - items - start'>" +
-          "< div class= 'd-flex w-100 justify-content-between'><h5 class='mb-1'>" + data[i].Headline + 
-          "</h5><small> 3 days ago</small></div><p class='mb-1'>" + data[i].Summary +
-          "</p></a></div>");
+          "<div class='list-group'>" + 
+            "<span class='list-group-item list-group-item-action flex-column align-items-start'>" +
+              "<div class= 'd-flex w-100 justify-content-between'>" + 
+                "<a href='" + data[i].URL + "'><h5 class='mb-1'>" + data[i].Headline + "</h5></a>" + 
+                "<button type='button' class='btn btn-dark btn-sm' id='save'> Save </button>" +
+              "</div>" + 
+              "<p class='mb-1'>" + data[i].Summary + "</p>" + 
+            "</span>" + 
+          "</div>");
       }
     });
   });
+});
+
+$(document).on("click", "#save", function() {
+
 });
