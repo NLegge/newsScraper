20885db51018def79b4b30f9e8a6a4dea343c033
Your NameNLegge <little.legge@gmail.com>
2017-12-01 15:44:54 -0700

    Making progress on scrape error. Scrape working, but returning articles multiple times in a weird format

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4,9 +4,18 @@ $(document).on("click", "#scrape", function() {
     method: "GET",
     url: "/scrape"
   }).done(function () {
-    // Log the response
-    console.log("Scrape Complete");
-    // Reload
-    location.reload();
+
+    $.getJSON("/articles", function (data) {
+      // For each one
+      for (let i = 0; i < data.length; i++) {
+        // Display the apropos information on the page
+        $("#articles").append(
+          "<div class='list - group'></div><a href=" + data[i].URL + 
+          "class='list - group - item list - group - item - action flex - column align - items - start'>" +
+          "< div class= 'd-flex w-100 justify-content-between'><h5 class='mb-1'>" + data[i].Headline + 
+          "</h5><small> 3 days ago</small></div><p class='mb-1'>" + data[i].Summary +
+          "</p></a></div>");
+      }
+    });
   });
 });
--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get("/scrape", function (req, res) {
     var $ = cheerio.load(response.data);
 
     // Now, we grab every article tag, and do the following:
-    $("article").each(function (i, element) {
+    $("div.main__content article").each(function (i, element) {
       // Save an empty result object
       var result = {};
 
@@ -66,8 +66,8 @@ app.get("/scrape", function (req, res) {
       db.Articles
         .create(result)
         .then(function (dbArticles) {
-          // If we were able to successfully scrape and save an Article, send a message to the client
-          res.json(dbArticles);
+          // If we were able to successfully scrape and save an Article
+          res.send("Scrape Complete");
       })
         .catch(function (err) {
           // If an error occurred, send it to the client
@@ -75,6 +75,21 @@ app.get("/scrape", function (req, res) {
         });
     });
   });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function (req, res) {
+  // Grab every document in the Articles collection
+  db.Articles
+    .find({})
+    .then(function (dbArticles) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticles);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
 });
 
 // Render saved articles handlebars page
